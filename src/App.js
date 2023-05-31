f5f325861a9a069b7de5e4b6cccb869ba5305fc9
John <jshar15089@gmail.com>
2023-05-30 20:32:47 -0400

    added createBook feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-function App(){
-    return(
-        <div>
-            App
-        </div>
-    )
+import { useState } from "react";
+import BookCreate from "./Components/BookCreate";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Need to add book with:", title);
+  };
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
-export default App
+export default App;
