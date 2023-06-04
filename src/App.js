becf9f9f6aa932e238b1711002dbb0897f6e4424
JohnsHarrison <jshar15089@gmail.com>
2023-06-04 15:43:25 -0400

    added images edit feature and images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import BookList from "./Components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id,newTitle) =>{
+    const updatedBooks = books.map((book)=>{
+      if(book.id === id){
+        return {...book,title:newTitle}
+      }
+      return book
+    })
+    setBooks(updatedBooks)
+  }
+
   const deleteBookById = (id) =>{
     const updatedBooks = books.filter((book)=>{
       return book.id !== id
@@ -21,7 +31,8 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} onDelete={deleteBookById} />
+      <h1>Reading List</h1>
+      <BookList books={books} onEdit={editBookById} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
